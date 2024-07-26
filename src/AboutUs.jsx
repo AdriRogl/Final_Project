fa0d2ef213f9ab4482fdc894bb69d13f50890a93
Adriana <aarogel@puce.edu.ec>
2024-07-25 19:42:23 -0500

    Implementación de página AboutUs

---
+++ src/AboutUs.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./aboutus.css";
+
+const AboutUs = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="about-container">
+      <h1>Peacepals</h1>
+      <p>
+        Nuestra app se creó para poder darle un alivio a las personas que están
+        pasando por momentos difíciles.
+      </p>
+      <div className="team">
+        <div className="member">
+          <img src="Images/Adri.jpeg" />
+          <h3>Adriana Rogel </h3>
+          <p>Encargada de las conexiones de las interfaces de la página</p>
+        </div>
+        <div className="member">
+          <img src="Images/Mateo.jpeg" />
+          <h3>Mateo Saenz</h3>
+          <p>Encargado del diseño responsive y estética de la página.</p>
+        </div>
+        <div className="member">
+          <img src="Images/Joss.jpeg" />
+          <h3>Josselyn Simbaña</h3>
+          <p>Encargada del diseño del juego y estética de la página</p>
+        </div>
+      </div>
+      <button className="back-button" onClick={() => navigate("/")}>
+        Volver
+      </button>
+    </div>
+  );
+};
+
+export default AboutUs;
